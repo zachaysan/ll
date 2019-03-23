ed40a72a9849382fe58bd2540903316f5f46ef86
Zach Aysan <zachaysan@gmail.com>
2019-03-23 06:04:01 -0700

    Add fixtures to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,23 @@ begin
   require "pry"
 rescue LoadError
 end
+
+class LL::Fixtures
+
+  def self.read_checklist version
+    File.read File.join("test", "fixtures", "checklists", version)
+  end
+
+  # Keep these staggered
+  def self.checklist
+    self.read_checklist "v0.0.2.json"
+  end
+  def self.previous_checklist
+    self.read_checklist "v0.0.2.json"
+  end
+
+  def self.latest_checklist
+    self.checklist
+  end
+
+end
