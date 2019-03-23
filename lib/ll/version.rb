397a7b9ac20230f18b097f2ecee029fd20055f1b
Zach Aysan <zachaysan@gmail.com>
2019-03-23 06:00:01 -0700

    Make version easy to call in a hurry

--- lib/ll/version.rb
+++ lib/ll/version.rb
@@ -1,3 +1,21 @@
 module LL
+
   VERSION = '0.0.3'
+  Version = VERSION
+
+  def version
+    LL::VERSION
+  end
+  module_function :version
+
+  def Version
+    LL::VERSION
+  end
+  module_function :Version
+
+  def VERSION
+    LL::VERSION
+  end
+  module_function :VERSION
+
 end
